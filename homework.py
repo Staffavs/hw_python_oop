823ce2185d6207201383245319810ce2138eb6cf
Staffavs <staffavs@gmail.com>
2020-11-21 14:49:18 +0300

    OOP Homework V1.1

--- homework.py
+++ homework.py
@@ -1,12 +1,9 @@
 import datetime as dt
 
-
 class Record:
     def __init__(self, amount, comment, date=None):
-
         if date is None:
             date = dt.datetime.date(dt.datetime.now())
-
         else:
             date = dt.datetime.strptime(date, '%d.%m.%Y').date()
         self.amount = amount
@@ -45,21 +42,13 @@ class Calculator:
         return result
 
     def get_remained(self):
-        result = super(self.get_today_stats())
-        print (f'{self.limit} - {result}')
-
+        result = self.limit - self.get_today_stats()
+        return result
 
 
 class CaloriesCalculator(Calculator):
-    def get_today_stats(self):
-        return (f'Сегодня уже наели на: {stats} кКал')
-
-    def get_week_stats(self):
-        return (f'За последние 7 дней получено: {week_stats} кКал')
-
     def get_calories_remained(self):
-        result = (super().get_today_stats())
-        result = self.limit - result
+        result = self.get_remained()
         if result > 0:
             return (f'Сегодня можно съесть что-нибудь ещё,'
                    f' но с общей калорийностью не более {result} кКал')
@@ -70,35 +59,22 @@ class CashCalculator(Calculator):
     USD_RATE = float(76.23)
     EURO_RATE = float(90.27)
 
-    def get_today_stats(self):
-        today_stats = (super().get_today_stats())
-        return (f'Сегодня потрачено: {today_stats} руб')
-
     def get_today_cash_remained(self, currency):
-        currencies = {
-            'rub': [1, 'руб'],
-            'usd': [self.USD_RATE, 'USD'],
-            'eur': [self.EURO_RATE, 'Euro']
-        }
-        rate = currencies[currency]
-        money = self.limit - float(super().get_today_stats())
-        money = round(money / rate[0], 2)
-        if money > 0:
-            return (f'На сегодня осталось {money} {rate[1]}')
-        elif money == 0:
+        money = self.get_remained()
+        if money == 0:
             return ('Денег нет, держись')
-        money = money * -1
-        return (f'Денег нет, держись: твой долг - {money} {rate[1]}')
 
-    def get_week_stats(self):
-        week_stats = (super().get_week_stats())
-        return (f'За последние 7 дней потрачено: {week_stats} руб')
+        currencies = (
+            ('usd', ('USD', CashCalculator.USD_RATE)),
+            ('eur', ('Euro', CashCalculator.EURO_RATE)),
+            ('rub', ('руб', 1)),
+        )
 
-cash_calculator = CashCalculator(1000)
-callo_calculator = CaloriesCalculator(2200)
-
-cash_calculator.add_record(Record(amount=145, comment="кофе"))
-cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
-cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
-
-cash_calculator.get_remained()
+        for i in currencies:
+            if i[0] == currency:
+                rate_name, rate = i[1]
+        money = round(money / rate, 2)
+        if money > 0:
+            return (f'На сегодня осталось {money} {rate_name}')
+        money = abs(money)
+        return (f'Денег нет, держись: твой долг - {money} {rate_name}')
